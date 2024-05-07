edf3523a96f8a7f832df8692984ead2c05d540af
paulinnleao <phleaoliveira@gmail.com>
2024-05-07 11:03:47 -0300

    Encontrei o erro, eu acho. '.'

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,96 +3,7 @@ import { useEffect, useMemo, useState } from 'react';
 import '../Styles/Game.css'
 import '../Styles/Card.css'
 
-// import {Images} from "./Images";
-
-
-
-
-
-
-
-
-
-
-
-
-
-import global from '../Images/aquecimento-global.png';
-import stick from '../Images/bastao.png';
-import binoculars from '../Images/binoculos.png';
-import coffin from '../Images/caixao.png';
-import camera from '../Images/camera-cinematografica.png';
-import rain from '../Images/chuva.png';
-import elixir from '../Images/elixir.png';
-import flamenco from '../Images/flamenco.png';
-import cat from '../Images/gato-preto.png';
-import pumpkin from '../Images/jack-o-lanterna.png';
-import moon from '../Images/lua-cheia.png';
-import museum from '../Images/museu.png';
-import sunrise from '../Images/nascer-do-sol.png';
-import sun from '../Images/sol.png';
-import mat from '../Images/tapete-vermelho.png';
-import vase from '../Images/vaso.png';
-
-const Images = [
-            {image: global, status: false, found: false},
-            {image: global, status: false, found: false},
-            {image: stick, status: false, found: false},
-            {image: stick, status: false, found: false},
-            {image: binoculars, status: false, found: false},
-            {image: binoculars, status: false, found: false},
-            {image: coffin, status: false, found: false},
-            {image: coffin, status: false, found: false},
-            {image: camera, status: false, found: false},
-            {image: camera, status: false, found: false},
-            {image: rain, status: false, found: false},
-            {image: rain, status: false, found: false},
-            {image: elixir, status: false, found: false},
-            {image: elixir, status: false, found: false},
-            {image: flamenco, status: false, found: false},
-            {image: flamenco, status: false, found: false},
-            {image: cat, status: false, found: false},
-            {image: cat, status: false, found: false},
-            {image: pumpkin, status: false, found: false},
-            {image: pumpkin, status: false, found: false},
-            {image: moon, status: false, found: false},
-            {image: moon, status: false, found: false},
-            {image: museum, status: false, found: false},
-            {image: museum, status: false, found: false},
-            {image: sunrise, status: false, found: false},
-            {image: sunrise, status: false, found: false},
-            {image: sun, status: false, found: false},
-            {image: sun, status: false, found: false},
-            {image: mat, status: false, found: false},
-            {image: mat, status: false, found: false},
-            {image: vase, status: false, found: false},
-            {image: vase, status: false, found: false},
-        ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import {Images} from "./Images";
 
 import thisPunch from '../songs/punch.mp3';
 import thisSoda from '../songs/soda.mp3';
@@ -175,10 +86,9 @@ const Game = ({data, setData}) => {
                 {imageToPlay.map((value, i) => (
                     <img 
                      key={i}
-                     src={value.status ? '.'+value.image : BackgroundImageCard}
+                     src={value.status ? value.image : BackgroundImageCard}
                      className={value.status ? 'showCard' : 'hideCard'}
                      onClick={()=>{
-                        console.log(value.image)
                         if(verifyCards.length<2 && !value.status){
                             if(!value.found){
                                 punch.play();
